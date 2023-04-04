75c7bfd85ecc4db62045390aa6d8b496c6e73fc0
n.lavrentiev <lavrentiev.nikita@gmail.com>
2023-04-04 23:05:43 +0300

    Переписал код теперь можно работать с Hibernate

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,19 +1,29 @@
 package models;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "first_name")
     private String first_name;
+    @Column(name = "last_name")
     private String last_name;
+    @Column(name = "gender")
     private String gender;
+    @Column(name = "age")
     private int age;
-    private City city;
+    @Column(name = "city_id")
+    private int city;
 
     public Employee() {
     }
 
-    public Employee(int id, String first_name, String last_name, String gender, int age, City city) {
+    public Employee(int id, String first_name, String last_name, String gender, int age, int city) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
@@ -22,12 +32,16 @@ public class Employee {
         this.city = city;
     }
 
-    public Employee(String first_name, String last_name, String gender, int age, City city) {
+    public Employee(String first_name, String last_name, String gender, int age, int city) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.gender = gender;
         this.age = age;
         this.city = city;
+    }
+
+    public Employee(int id) {
+        this.id = id;
     }
 
     public int getId() {
@@ -70,11 +84,11 @@ public class Employee {
         this.age = age;
     }
 
-    public City getCity() {
+    public int getCity() {
         return city;
     }
 
-    public void setCity(City city) {
+    public void setCity(int city) {
         this.city = city;
     }
 
@@ -99,9 +113,8 @@ public class Employee {
                 ", last_name='" + last_name + '\'' +
                 ", gender='" + gender + '\'' +
                 ", age=" + age +
-                ", city=" + city +
+                ", city_id=" + city +
                 '}';
     }
-
 }
 
